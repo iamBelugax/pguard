17b7595cfa06ab2189646abc9e8cafc0448cd8ff
iamBelugaa <iamnilotpaldeka@gmail.com>
2026-01-11 13:08:49 +0530

    add basic pguard command runner to exec and forward stdout/stderr

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("pguard: invalid args")
+	}
+
+	_ = os.Args[1]
+	cmdName := os.Args[2]
+	cmdArgs := os.Args[3:]
+
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalln("pguard:", err)
+	}
+}
